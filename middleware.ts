2f3baea6dd108072ff65424b353d1a6025b42b42
Daniel Tostes <tostes.dev@gmail.com>
2024-05-14 16:09:21 -0300

    Format all files with prettier

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };

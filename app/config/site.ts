160a7ee67132c11bf7f34abc06f2f7a0a5be76e5
Daniel Tostes <tostes.dev@gmail.com>
2024-05-28 14:53:48 -0300

    feat: siteConfig

---
+++ app/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "Rust Conveyor Filters",
+  description:
+    "Generate Rust conveyor filters with ease. Create, edit, and share your filters with others.",
+  links: {
+    gitHub: "https://github.com/tostesdaniel/rust-conveyor-filters",
+  },
+};
+
+export type siteConfig = typeof siteConfig;

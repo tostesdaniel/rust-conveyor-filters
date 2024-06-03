6543ede1432407c6a6ffb312087ca83dd4d2e81a
Daniel Tostes <tostes.dev@gmail.com>
2024-06-03 16:32:52 -0300

    feat: add S3 as remotePattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: `${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+      },
+    ],
+  },
+};
 
 export default nextConfig;

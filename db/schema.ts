53ae4d57867b01791fc9c87f8af58bf53b6aaf05
Daniel Tostes <tostes.dev@gmail.com>
2024-06-13 08:25:25 -0300

    refactor: change collections table name to filters

--- db/schema.ts
+++ db/schema.ts
@@ -20,54 +20,51 @@ export type Item = typeof items.$inferSelect;
 export type NewItem = typeof items.$inferInsert;
 
 export const itemsRelations = relations(items, ({ many }) => ({
-  itemsToCollections: many(itemsToCollections),
+  itemsToFilters: many(itemsToFilters),
 }));
 
-export const collections = pgTable("collections", {
+export const filters = pgTable("filters", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }).notNull(),
   authorId: varchar("author_id", { length: 256 }).notNull(),
   imagePath: varchar("image_path", { length: 256 }).notNull(),
 });
 
-export const collectionsRelations = relations(collections, ({ many }) => ({
-  itemsToCollections: many(itemsToCollections),
+export const filtersRelations = relations(filters, ({ many }) => ({
+  itemsToFilters: many(itemsToFilters),
 }));
 
-export type Collection = typeof collections.$inferSelect;
-export type NewCollection = typeof collections.$inferInsert;
+export type Filter = typeof filters.$inferSelect;
+export type NewFilter = typeof filters.$inferInsert;
 
-export const itemsToCollections = pgTable(
-  "items_to_collections",
+export const itemsToFilters = pgTable(
+  "items_to_filters",
   {
     itemId: integer("item_id")
       .notNull()
       .references(() => items.id),
-    collectionId: integer("collection_id")
+    filterId: integer("filter_id")
       .notNull()
-      .references(() => collections.id),
+      .references(() => filters.id),
   },
   (t) => ({
-    pk: primaryKey({ columns: [t.itemId, t.collectionId] }),
+    pk: primaryKey({ columns: [t.itemId, t.filterId] }),
   }),
 );
 
-export const itemsToCollectionsRelations = relations(
-  itemsToCollections,
-  ({ one }) => ({
-    collections: one(collections, {
-      fields: [itemsToCollections.collectionId],
-      references: [collections.id],
-    }),
-    items: one(items, {
-      fields: [itemsToCollections.itemId],
-      references: [items.id],
-    }),
+export const itemsToFiltersRelations = relations(itemsToFilters, ({ one }) => ({
+  filters: one(filters, {
+    fields: [itemsToFilters.filterId],
+    references: [filters.id],
   }),
-);
+  items: one(items, {
+    fields: [itemsToFilters.itemId],
+    references: [items.id],
+  }),
+}));
 
-export type ItemToCollection = typeof itemsToCollections.$inferSelect;
-export type NewItemToCollection = typeof itemsToCollections.$inferInsert;
+export type ItemToFilter = typeof itemsToFilters.$inferSelect;
+export type NewItemToFilter = typeof itemsToFilters.$inferInsert;
 
 export const categories = pgTable("categories", {
   id: integer("id").primaryKey(),

942f41ce165b0a170a9a46c779a2180c8c2289b0
Daniel Tostes <tostes.dev@gmail.com>
2024-06-23 22:56:05 -0300

    feat: user filters list

---
+++ components/my-filters.tsx
@@ -0,0 +1,14 @@
+import { FilterWithItemIds } from "@/types/filter";
+
+import { MyFilterCard as FilterCard } from "./my-filter-card";
+
+export function MyFilters({ filters }: { filters: FilterWithItemIds[] }) {
+  return (
+    <ul
+      role='list'
+      className='mt-6 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4'
+    >
+      {filters?.map((filter) => <FilterCard key={filter.id} filter={filter} />)}
+    </ul>
+  );
+}

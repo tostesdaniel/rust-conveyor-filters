843ab9b8988d6ade4b52c52a70be54a424402f56
Daniel Tostes <tostes.dev@gmail.com>
2024-06-24 15:12:27 -0300

    feat: new types for filter items with item relation

--- types/filter.ts
+++ types/filter.ts
@@ -1,7 +1,15 @@
-import { Filter, FilterItem } from "@/db/schema";
+import { Filter, FilterItem, Item } from "@/db/schema";
 
 export type FilterWithItems = Filter & {
   filterItems: FilterItem[];
+};
+
+export type FilterItemWithItemInfo = FilterItem & {
+  item: Item;
+};
+
+export type FilterWithItemsAndInfo = Filter & {
+  filterItems: FilterItemWithItemInfo[];
 };
 
 export type FilterWithItemIds = Filter & {

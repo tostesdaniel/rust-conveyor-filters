8af8159086e17c4256fba94dcb25e03723d733e6
Daniel Tostes <tostes.dev@gmail.com>
2024-06-24 23:34:20 -0300

    refactor: update prop type to use only item info

--- lib/export-conveyor-filter.ts
+++ lib/export-conveyor-filter.ts
@@ -1,18 +1,16 @@
-import { FilterWithItemsAndInfo } from "@/types/filter";
+import { FilterItemWithItemInfo, FilterWithItemsAndInfo } from "@/types/filter";
 import { GameConveyorFilterItem } from "@/types/gameitem";
 
-export function exportConveyorFilter(filter: FilterWithItemsAndInfo) {
-  const conveyorFilter: GameConveyorFilterItem[] = filter.filterItems.map(
-    (filterItem) => ({
-      TargetCategory: null,
-      MaxAmountInOutput: filterItem.max,
-      BufferAmount: filterItem.buffer,
-      MinAmountInInput: filterItem.min,
-      IsBlueprint: false,
-      BufferTransferRemaining: 0,
-      TargetItemName: filterItem.item.shortname,
-    }),
-  );
+export function exportConveyorFilter(filter: FilterItemWithItemInfo[]) {
+  const conveyorFilter: GameConveyorFilterItem[] = filter.map((filterItem) => ({
+    TargetCategory: null,
+    MaxAmountInOutput: filterItem.max,
+    BufferAmount: filterItem.buffer,
+    MinAmountInInput: filterItem.min,
+    IsBlueprint: false,
+    BufferTransferRemaining: 0,
+    TargetItemName: filterItem.item.shortname,
+  }));
 
   return JSON.stringify(conveyorFilter, null, 2);
 }

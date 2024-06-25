52ba014a94698b01891f83d820d35b92db4f9d22
Daniel Tostes <tostes.dev@gmail.com>
2024-06-24 23:35:55 -0300

    refactor: add conditional rendering for button or dropdownMenuItem

--- components/export-conveyor-filter.tsx
+++ components/export-conveyor-filter.tsx
@@ -2,12 +2,22 @@
 
 import { toast } from "sonner";
 
+import { FilterItemWithItemInfo } from "@/types/filter";
 import { useCopyToClipboard } from "@/hooks/use-copy-to-clipboard";
 import { exportConveyorFilter } from "@/lib/export-conveyor-filter";
 
+import { Button } from "./ui/button";
 import { DropdownMenuItem } from "./ui/dropdown-menu";
 
-export function ExportConveyorFilter({ filter }: any) {
+interface ExportConveyorFilterProps {
+  type: "button" | "dropdown";
+  filter: FilterItemWithItemInfo[];
+}
+
+export function ExportConveyorFilter({
+  type,
+  filter,
+}: ExportConveyorFilterProps) {
   const [_copiedText, copy] = useCopyToClipboard();
 
   const handleCopy = (text: string) => () => {
@@ -20,7 +30,15 @@ export function ExportConveyorFilter({ filter }: any) {
       });
   };
 
-  return (
+  return type === "button" ? (
+    <Button
+      type='button'
+      onClick={() => handleCopy(exportConveyorFilter(filter))}
+      size='sm'
+    >
+      Export
+    </Button>
+  ) : (
     <DropdownMenuItem onSelect={handleCopy(exportConveyorFilter(filter))}>
       Export
     </DropdownMenuItem>

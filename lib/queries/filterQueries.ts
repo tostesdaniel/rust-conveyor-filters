a09953d5f9d793cd2b5434cdb95c1f9114ab1a25
Daniel Tostes <tostes.dev@gmail.com>
2024-07-09 11:54:42 -0300

    refactor: remove unused function and add missing use server

--- lib/queries/filterQueries.ts
+++ lib/queries/filterQueries.ts
@@ -1,18 +1,7 @@
+"use server";
+
 import { db } from "@/db";
 import { eq } from "drizzle-orm";
-
-import { FilterWithItemIds } from "@/types/filter";
-
-export async function getFiltersById(
-  userId: string,
-): Promise<FilterWithItemIds[] | undefined> {
-  return await db.query.filters.findMany({
-    where: (filters) => eq(filters.authorId, userId),
-    with: {
-      filterItems: { columns: { itemId: true } },
-    },
-  });
-}
 
 export async function getFiltersWithItems(userId: string) {
   return await db.query.filters.findMany({

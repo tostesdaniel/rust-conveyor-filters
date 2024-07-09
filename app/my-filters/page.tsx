2e86796aeea215e54db3d75dab724fcb5bcef8bb
Daniel Tostes <tostes.dev@gmail.com>
2024-07-09 11:52:29 -0300

    refactor: move elements from page to MyFilters component
    Now uses TanStack Query for caching filters

--- app/my-filters/page.tsx
+++ app/my-filters/page.tsx
@@ -1,47 +1,32 @@
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
-import { FolderPlusIcon, PlusIcon } from "lucide-react";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 import { getFiltersWithItems } from "@/lib/queries";
-import { HeadingWithAction } from "@/components/ui/heading-with-action";
-import { Typography } from "@/components/ui/typography";
-import { EmptyState } from "@/components/empty-state";
-import { MyFilters as UserFilters } from "@/components/my-filters";
+import { MyFilters } from "@/components/my-filters";
 
-export default async function MyFilters() {
+export default async function MyFiltersPage() {
   const { userId } = auth();
+  const queryClient = new QueryClient();
 
   if (!userId) {
     redirect("/sign-in");
   }
 
-  const filters = await getFiltersWithItems("1");
+  await queryClient.prefetchQuery({
+    queryKey: ["user-filters", userId],
+    queryFn: () => getFiltersWithItems(userId),
+  });
 
   return (
     <>
-      {filters?.length ? (
-        <HeadingWithAction
-          buttonLabel='New Filter'
-          label='My Filters'
-          redirectUrl='/my-filters/new-filter'
-          variant='h1'
-          ActionIcon={PlusIcon}
-        />
-      ) : (
-        <Typography variant='h1'>New Filter</Typography>
-      )}
-      {filters?.length ? (
-        <UserFilters filters={filters} />
-      ) : (
-        <EmptyState
-          Icon={FolderPlusIcon}
-          title='No filters'
-          description='Get started by creating a new filter.'
-          label='New Filter'
-          ButtonIcon={PlusIcon}
-          redirectUrl='/my-filters/new-filter'
-        />
-      )}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <MyFilters userId={userId} />
+      </HydrationBoundary>
     </>
   );
 }

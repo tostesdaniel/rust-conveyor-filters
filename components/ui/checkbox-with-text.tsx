41fcdd4782fd9a08ff3b78600dd594479d84f015
Daniel Tostes <tostes.dev@gmail.com>
2024-07-10 19:18:38 -0300

    feat: checkbox with text component

---
+++ components/ui/checkbox-with-text.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import * as React from "react";
+
+import { Checkbox } from "./checkbox";
+import { FormControl, FormDescription, FormItem, FormLabel } from "./form";
+
+interface CheckBoxWithDescriptionProps {
+  checked: boolean;
+  onCheckedChange: (checked: boolean) => void;
+  label: string;
+  description?: React.ReactNode;
+}
+
+export function CheckboxWithDescription({
+  checked,
+  onCheckedChange,
+  label,
+  description,
+}: CheckBoxWithDescriptionProps) {
+  return (
+    <FormItem className='flex items-center space-x-3 space-y-0'>
+      <FormControl>
+        <Checkbox checked={checked} onCheckedChange={onCheckedChange} />
+      </FormControl>
+      <FormLabel>{label}</FormLabel>
+      {description && <FormDescription>{description}</FormDescription>}
+    </FormItem>
+  );
+}

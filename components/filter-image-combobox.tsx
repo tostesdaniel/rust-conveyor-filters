7cc7672daa9758b2054e4bf728097ac869f36647
Daniel Tostes <tostes.dev@gmail.com>
2024-07-10 17:46:48 -0300

    feat: filter image selection combobox

---
+++ components/filter-image-combobox.tsx
@@ -0,0 +1,179 @@
+"use client";
+
+import * as React from "react";
+import Image from "next/image";
+import { ChevronsUpDown } from "lucide-react";
+import { ControllerRenderProps, useFormContext } from "react-hook-form";
+
+import { useMediaQuery } from "@/hooks/use-media-query";
+import { cn } from "@/lib/utils";
+import { type Item } from "@/db/schema";
+
+import { FilterImage } from "./filter-image";
+import { Button } from "./ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/command";
+import { Drawer, DrawerContent, DrawerTrigger } from "./ui/drawer";
+import { FormControl } from "./ui/form";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+
+interface FilterImageComboboxProps {
+  field: ControllerRenderProps<any, any>;
+  items: Item[];
+}
+
+type SelectedImage = string;
+const FilterImageCombobox = React.memo(
+  ({ field, items }: FilterImageComboboxProps) => {
+    const [open, setOpen] = React.useState(false);
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+    const [selectedImage, setSelectedImage] = React.useState<SelectedImage>(
+      field.value,
+    );
+
+    React.useEffect(() => {
+      setSelectedImage(field.value);
+    }, [field.value]);
+
+    const handleSelect = React.useCallback(
+      (imagePath: string) => {
+        setSelectedImage(imagePath);
+        setOpen(false);
+        field.onChange(imagePath);
+      },
+      [field],
+    );
+
+    if (isDesktop) {
+      return (
+        <div className='flex items-center gap-x-3'>
+          <FilterImage imagePath={selectedImage} />
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <FormControl>
+                <Button
+                  variant='outline'
+                  role='combobox'
+                  aria-expanded={open}
+                  className={cn(
+                    "w-[300px] justify-between",
+                    !field.value && "text-muted-foreground",
+                  )}
+                >
+                  {field.value
+                    ? items.find((item) => item.imagePath === field.value)?.name
+                    : "Select item"}
+                  <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className='w-fit p-0' align='start'>
+              <ItemList
+                items={items}
+                setOpen={setOpen}
+                setSelectedImage={handleSelect}
+                field={field}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+      );
+    }
+
+    return (
+      <Drawer open={open} onOpenChange={setOpen}>
+        <div className='flex items-center gap-x-3'>
+          <FilterImage imagePath={selectedImage} />
+          <DrawerTrigger asChild>
+            <Button
+              variant='outline'
+              role='combobox'
+              aria-expanded={open}
+              className={cn(
+                "w-[300px] justify-between",
+                !field.value && "text-muted-foreground",
+              )}
+            >
+              {field.value
+                ? items.find((item) => item.imagePath === field.value)?.name
+                : "Select item"}
+              <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+            </Button>
+          </DrawerTrigger>
+        </div>
+        <DrawerContent>
+          <div className='mt-4 border-t'>
+            <ItemList
+              items={items}
+              setOpen={setOpen}
+              setSelectedImage={handleSelect}
+              field={field}
+            />
+          </div>
+        </DrawerContent>
+      </Drawer>
+    );
+  },
+);
+
+FilterImageCombobox.displayName = "FilterImageCombobox";
+
+interface ItemListProps {
+  items: Item[];
+  setOpen: (open: boolean) => void;
+  setSelectedImage: (image: string) => void;
+  field: ControllerRenderProps<any, any>;
+}
+
+const ItemList = React.memo(
+  ({ items, setOpen, setSelectedImage, field }: ItemListProps) => {
+    const form = useFormContext();
+
+    return (
+      <Command>
+        <CommandInput placeholder='Filter items...' />
+        <CommandList>
+          <CommandEmpty>Item does not exist.</CommandEmpty>
+          <CommandGroup>
+            {items.map((item) => (
+              <CommandItem
+                key={item.itemId}
+                className='flex items-center gap-x-2'
+                value={item.name}
+                onSelect={() => {
+                  form.setValue("imagePath", item.imagePath, {
+                    shouldTouch: true,
+                  });
+                  setSelectedImage(item.imagePath);
+                  setOpen(false);
+                  field.onChange(item.imagePath);
+                }}
+              >
+                <div className='relative h-6 w-6'>
+                  <Image
+                    src={`/items/${item.imagePath}.png`}
+                    alt={item.name}
+                    width={24}
+                    height={24}
+                    className='rounded-sm object-contain'
+                  />
+                </div>
+                {item.name}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    );
+  },
+);
+
+ItemList.displayName = "ItemList";
+
+export { FilterImageCombobox };

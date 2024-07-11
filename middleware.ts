e843cf43f631c0a2a5e61b746f6892700e80582c
Daniel Tostes <tostes.dev@gmail.com>
2024-07-11 12:19:20 -0300

    feat(auth): set all /my-filters routes as protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+const isProtectedRoute = createRouteMatcher(["/my-filters(.*)"]);

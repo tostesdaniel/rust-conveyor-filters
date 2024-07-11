b21a51954c8dd2df64ba34ae6d79a7682d3aad9b
Daniel Tostes <tostes.dev@gmail.com>
2024-07-11 14:35:14 -0300

    feat: getItems action

---
+++ actions/itemActions.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { db } from "@/db";
+import { auth } from "@clerk/nextjs/server";
+import { asc } from "drizzle-orm";
+
+import { items } from "@/db/schema";
+
+export async function getItems() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return {
+      success: false,
+      error: {
+        message: "Unauthorized",
+      },
+    };
+  }
+
+  try {
+    const result = await db
+      .select()
+      .from(items)
+      .orderBy(asc(items.category), asc(items.name));
+    return { success: true, data: result };
+  } catch (error) {
+    return {
+      success: false,
+      error: {
+        message: "Failed to get items",
+      },
+    };
+  }
+}
+

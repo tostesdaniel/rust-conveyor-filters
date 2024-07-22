3d6ed250c98711976b583b1e7bb31987a6b7e9a3
Daniel Tostes <tostes.dev@gmail.com>
2024-07-22 01:25:51 -0300

    feat: remove unwanted prefetchQuery
    # Reason:
    
    - Data is already fetched at MyFilters component

--- app/(app)/my-filters/page.tsx
+++ app/(app)/my-filters/page.tsx
@@ -1,32 +1,18 @@
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
 
-import { getFiltersWithItems } from "@/lib/queries";
 import { MyFilters } from "@/components/my-filters";
 
 export default async function MyFiltersPage() {
   const { userId } = auth();
-  const queryClient = new QueryClient();
 
   if (!userId) {
     redirect("/sign-in");
   }
 
-  await queryClient.prefetchQuery({
-    queryKey: ["user-filters", userId],
-    queryFn: () => getFiltersWithItems({ userId }).then(([data]) => data),
-  });
-
   return (
     <>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <MyFilters userId={userId} />
-      </HydrationBoundary>
+      <MyFilters userId={userId} />
     </>
   );
 }

3b86a15894b98aa4625eb904ac715d963557eb5d
Daniel Tostes <tostes.dev@gmail.com>
2024-07-23 20:17:26 -0300

    feat: prefetch users filters and remove imperative redirect

--- src/app/(app)/my-filters/page.tsx
+++ src/app/(app)/my-filters/page.tsx
@@ -1,18 +1,28 @@
-import { redirect } from "next/navigation";
-import { auth } from "@clerk/nextjs/server";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
+import { getFiltersWithItems } from "@/lib/queries";
 import { MyFilters } from "@/components/my-filters";
 
 export default async function MyFiltersPage() {
-  const { userId } = auth();
+  const queryClient = new QueryClient();
 
-  if (!userId) {
-    redirect("/sign-in");
-  }
+  await queryClient.prefetchQuery({
+    queryKey: ["user-filters"],
+    queryFn: async () => {
+      const [data] = await getFiltersWithItems();
+      return data;
+    },
+  });
 
   return (
     <>
+      <HydrationBoundary state={dehydrate(queryClient)}>
       <MyFilters />
+      </HydrationBoundary>
     </>
   );
 }

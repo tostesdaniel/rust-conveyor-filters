693d1d691637783e7c1d2fccb4cb6e0e34f066bf
Daniel Tostes <tostes.dev@gmail.com>
2024-07-28 02:54:58 -0300

    feat: add url and LinkedIn link

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,9 +1,11 @@
 export const siteConfig = {
   name: "Rust Conveyor Filters",
+  url: "https://rust-conveyor-filters.vercel.app",
   description:
     "Generate Rust conveyor filters with ease. Create, edit, and share your filters with others.",
   links: {
     gitHub: "https://github.com/tostesdaniel/rust-conveyor-filters",
+    linkedIn: "https://www.linkedin.com/in/danieltostes/",
   },
 };
 

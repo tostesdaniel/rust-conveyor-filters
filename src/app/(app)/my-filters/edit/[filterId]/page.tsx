9fed2b4569a9747c7d0b8a0fa670f65569ac1405
Daniel Tostes <tostes.dev@gmail.com>
2024-07-28 20:21:31 -0300

    feat: EditFilter page dynamic metadata

--- src/app/(app)/my-filters/edit/[filterId]/page.tsx
+++ src/app/(app)/my-filters/edit/[filterId]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import {
   dehydrate,
   HydrationBoundary,
@@ -5,8 +6,30 @@ import {
 } from "@tanstack/react-query";
 
 import { getItems } from "@/actions/itemActions";
+import { getUserFilterById } from "@/lib/queries";
 import { Typography } from "@/components/ui/typography";
 import { EditFilterForm } from "@/app/(app)/my-filters/edit/[filterId]/edit-filter-form";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { filterId: string };
+}): Promise<Metadata> {
+  const [filter] = await getUserFilterById({
+    filterId: Number(params.filterId),
+  });
+
+  if (!filter) {
+    return {
+      title: "Filter not found",
+    };
+  }
+
+  return {
+    title: `Editing: ${filter.name}`,
+    description: filter.description ?? undefined,
+  };
+}
 
 export default async function EditFilterPage({
   params,

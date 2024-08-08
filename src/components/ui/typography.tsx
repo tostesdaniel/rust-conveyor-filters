50bdca3866ed6b3d709f0fddf045b56770245c23
Daniel Tostes <tostes.dev@gmail.com>
2024-08-08 13:33:09 -0300

    feat: update h2 to have margin top

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -8,7 +8,7 @@ const typographyVariants = cva("text-foreground", {
   variants: {
     variant: {
       h1: "scroll-m-20 text-4xl font-bold tracking-tight",
-      h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+      h2: "mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
       h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
       h4: "scroll-m-20 text-xl font-semibold tracking-tight",
       p: "leading-7 [&:not(:first-child)]:mt-6",

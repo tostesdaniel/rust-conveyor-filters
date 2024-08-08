8aca4e343aa63542c549e6f2c8045fc6dedb4ac9
Daniel Tostes <tostes.dev@gmail.com>
2024-08-08 13:32:32 -0300

    feat: mdx-components

---
+++ src/mdx-components.tsx
@@ -0,0 +1,15 @@
+import type { MDXComponents } from "mdx/types";
+
+import { Typography } from "@/components/ui/typography";
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    h1: (props) => <Typography variant='h1' {...props} />,
+    h2: (props) => <Typography variant='h2' {...props} />,
+    h3: (props) => <Typography variant='h3' {...props} />,
+    h4: (props) => <Typography variant='h4' {...props} />,
+    p: (props) => <Typography variant='p' {...props} />,
+    ul: (props) => <Typography variant='ul' {...props} />,
+    ...components,
+  };
+}

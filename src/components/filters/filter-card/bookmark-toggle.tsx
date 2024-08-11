7e57c68b2b3b1a3ff24b4afdfb587104abd48c6f
Daniel Tostes <tostes.dev@gmail.com>
2024-08-10 21:27:07 -0300

    feat: bookmark toggle component

---
+++ src/components/filters/filter-card/bookmark-toggle.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import type { ToggleProps } from "@radix-ui/react-toggle";
+import { useQueryClient } from "@tanstack/react-query";
+import { BookmarkIcon, Loader2Icon } from "lucide-react";
+import { toast } from "sonner";
+
+import { bookmarkFilter, getBookmarkedStatus } from "@/actions/bookmark-filter";
+import {
+  useServerActionMutation,
+  useServerActionQuery,
+} from "@/hooks/server-action-hooks";
+import { Toggle } from "@/components/ui/toggle";
+
+interface BookmarkToggleProps extends ToggleProps {
+  filterId: number;
+  initialBookmarked?: boolean;
+}
+
+export function BookmarkToggle({
+  filterId,
+  initialBookmarked = false,
+  ...props
+}: BookmarkToggleProps) {
+  const { isLoaded, isSignedIn } = useUser();
+  const [isBookmarked, setIsBookmarked] = useState<boolean>(initialBookmarked);
+  const queryClient = useQueryClient();
+
+  const { data: bookmarkedData, isLoading } = useServerActionQuery(
+    getBookmarkedStatus,
+    {
+      input: { filterId },
+      queryKey: ["bookmarked", filterId],
+      enabled: isLoaded && isSignedIn && !initialBookmarked,
+    },
+  );
+
+  const mutation = useServerActionMutation(bookmarkFilter, {
+    onSuccess: (data) => {
+      setIsBookmarked(data.bookmarked);
+      toast.success(
+        data.bookmarked ? "Filter bookmarked" : "Filter unbookmarked",
+      );
+    },
+    onError: (err) => {
+      if (err.code === "NOT_AUTHORIZED") {
+        toast.error("You must be signed in to bookmark a filter");
+      } else {
+        toast.error("An error occurred while bookmarking the filter");
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["bookmarked-filters"] });
+    },
+  });
+
+  useEffect(() => {
+    if (bookmarkedData) {
+      setIsBookmarked(bookmarkedData.bookmarked);
+    }
+  }, [bookmarkedData]);
+
+  useEffect(() => {
+    if (initialBookmarked) {
+      setIsBookmarked(true);
+    }
+  }, [initialBookmarked]);
+
+  return (
+    <Toggle
+      aria-label='Bookmark filter'
+      className='group -my-3 hover:bg-transparent data-[state=on]:bg-transparent data-[state=on]:hover:text-muted-foreground'
+      onPressedChange={() => mutation.mutate({ filterId })}
+      pressed={isBookmarked}
+      {...props}
+    >
+      {isLoading ? (
+        <Loader2Icon className='h-4 w-4 animate-spin' />
+      ) : (
+        <BookmarkIcon className='h-4 w-4 group-data-[state=on]:fill-current' />
+      )}
+    </Toggle>
+  );
+}

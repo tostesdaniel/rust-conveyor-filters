2e7f43c75d6b1f16ea5b49e57ce9e8816942b5d4
Daniel Tostes <tostes.dev@gmail.com>
2024-08-17 02:23:05 -0300

    feat: add Steam configuration constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,3 +3,10 @@ export enum FilterSettingsFieldDescription {
   BUFFER = "Start transferring when this amount is in the container and transfer that amount exactly.",
   MIN = "Only transfer if there is this amount in the input container.",
 }
+
+const STEAM_GUIDE_ID = "3308184949";
+export const steamConfig = {
+  GUIDE_ID: STEAM_GUIDE_ID,
+  GUIDE_URL: `https://steamcommunity.com/sharedfiles/filedetails/?id=${STEAM_GUIDE_ID}`,
+  STEAM_ID: process.env.STEAM_ID,
+};

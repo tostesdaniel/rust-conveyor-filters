1ad2d2406fac63221ba063ff8c0ca3c1ca0c7b01
Daniel Tostes <tostes.dev@gmail.com>
2024-08-27 10:59:45 -0300

    feat: add WebSockets db connection

---
+++ src/db/pooled-connection.ts
@@ -0,0 +1,7 @@
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+
+import * as schema from "./schema";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+export const pooledDb = drizzle(pool, { schema });

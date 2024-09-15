1e9c09295d374298e3c80ca53a37e5feb05c6f9e
Daniel Tostes <tostes.dev@gmail.com>
2024-09-15 10:50:42 -0300

    feat: ClearInputButton component

---
+++ src/components/my-filters/new-filter/clear-input-button.tsx
@@ -0,0 +1,21 @@
+import { EraserIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+interface ClearInputButtonProps {
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+export function ClearInputButton({ setSearch }: ClearInputButtonProps) {
+  return (
+    <Button
+      type='button'
+      size='icon'
+      variant='ghost'
+      className='absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 opacity-50 hover:bg-transparent hover:opacity-100'
+      onClick={() => setSearch("")}
+    >
+      <EraserIcon className='h-4 w-4' />
+      <span className='sr-only'>Clear input</span>
+    </Button>
+  );
+}

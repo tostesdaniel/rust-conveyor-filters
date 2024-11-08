56135e65511e6a9ee37c6940fe366d47a09171ed
Daniel Tostes <tostes.dev@gmail.com>
2024-11-08 00:10:49 -0300

    feat(ui): update banner component with new dismiss button

--- src/components/ui/banner.tsx
+++ src/components/ui/banner.tsx
@@ -47,4 +47,29 @@ const BannerDescription = React.forwardRef<
 ));
 BannerDescription.displayName = "BannerDescription";
 
-export { Banner, BannerDescription, BannerTitle };
+const BannerDismiss = React.forwardRef<
+  HTMLButtonElement,
+  React.HTMLAttributes<HTMLButtonElement>
+>(({ className, ...props }, ref) => (
+  <div className='flex flex-1 justify-end'>
+    <button
+      ref={ref}
+      type='button'
+      className={cn("-m-3 p-3 focus-visible:outline-offset-[-4px]", className)}
+      {...props}
+    >
+      <span className='sr-only'>Dismiss</span>
+      <svg
+        className='h-5 w-5 text-foreground'
+        viewBox='0 0 20 20'
+        fill='currentColor'
+        aria-hidden='true'
+      >
+        <path d='M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z' />
+      </svg>
+    </button>
+  </div>
+));
+BannerDismiss.displayName = "BannerDismiss";
+
+export { Banner, BannerDescription, BannerDismiss, BannerTitle };

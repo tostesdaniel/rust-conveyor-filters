7f8e4abc563da64901f941470c6f9aaf63db6194
Daniel Tostes <tostes.dev@gmail.com>
2024-11-25 18:35:42 -0300

    refactor(conveyor-grid): enhance grid layout on small devices
    - Optimize grid layout on smaller devices
      - 1 column on minimal screens
      - 2 columns at 412px
      - Rest is unchanged

--- src/components/conveyor-item-grid.tsx
+++ src/components/conveyor-item-grid.tsx
@@ -25,7 +25,7 @@ export function ConveyorItemGrid({ items, onRemove }: ConveyorItemGridProps) {
   return (
     <ul
       role='list'
-      className='grid snap-y snap-mandatory grid-cols-2 gap-4 gap-x-4 gap-y-8 overflow-y-auto scroll-smooth p-1.5 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-5'
+      className='grid snap-y snap-mandatory grid-cols-1 gap-4 gap-x-4 gap-y-8 overflow-y-auto scroll-smooth p-1.5 min-[412px]:grid-cols-2 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-5'
     >
       {items?.map((item, index) => (
         <ConveyorItem

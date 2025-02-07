d6b0ca820cc833fdbbfdc3af23ff74a0a04fdfd9
Daniel Tostes <tostes.dev@gmail.com>
2025-02-07 01:56:00 -0300

    feat(ui): add hover effect and gradient border to hero image

--- src/components/landing-page/hero-image.tsx
+++ src/components/landing-page/hero-image.tsx
@@ -18,17 +18,21 @@ export function HeroImage() {
   }
 
   return (
-    <div className='mx-auto mt-16 flex max-w-2xl lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32'>
+    <div className='mx-auto mt-16 flex max-w-2xl lg:mt-0 lg:mr-0 lg:ml-10 lg:max-w-none lg:flex-none xl:ml-32'>
       <div className='max-w-3xl flex-none sm:max-w-5xl lg:max-w-none'>
-        <ThemeImage
-          srcLight='/images/hero-light.jpg'
-          srcDark='/images/hero-dark.jpg'
-          alt='Conveyor filter creator interface'
-          quality={100}
-          width={1408}
-          height={1024}
-          className='w-[70rem] rounded-md shadow-2xl ring-1 ring-foreground/10'
-        />
+        <div className='group relative'>
+          <div className='absolute -inset-px rounded-md bg-gradient-to-r from-[#4361ee] to-[#4cc9f0] blur transition-transform duration-500 ease-in-out group-hover:scale-[1.04]' />
+
+          <ThemeImage
+            srcLight='/images/hero-light.jpg'
+            srcDark='/images/hero-dark.jpg'
+            alt='Conveyor filter creator interface'
+            quality={100}
+            width={1408}
+            height={1024}
+            className='ring-border/10 relative w-[70rem] rounded-md ring-1 shadow-2xl transition-transform duration-500 ease-in-out group-hover:scale-[1.02]'
+          />
+        </div>
       </div>
     </div>
   );

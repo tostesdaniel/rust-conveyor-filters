6926d171dfb7739e0b718e17e489069b7b5c8d99
Daniel Tostes <tostes.dev@gmail.com>
2025-02-07 01:57:18 -0300

    feat(ui): enhance hero section with new badge and gradient text

--- src/components/landing-page/hero-content.tsx
+++ src/components/landing-page/hero-content.tsx
@@ -10,10 +10,29 @@ export function HeroContent() {
   return (
     <div className='mx-auto max-w-2xl shrink-0 lg:mx-0 lg:max-w-xl'>
       <div className='mt-8 sm:mt-12 lg:mt-16'>
-        <Typography variant='h1' className='font-extrabold sm:text-5xl'>
-          Streamline Your Rust Base Automation with Ease
+        <div className='border-border bg-background/80 hover:bg-background mb-8 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border px-7 py-2 shadow-md backdrop-blur transition-all'>
+          <p className='text-muted-foreground text-sm font-semibold'>
+            <span
+              role='img'
+              aria-label='Party Popper emoji'
+              className='select-none'
+            >
+              🎉
+            </span>{" "}
+            Now with filter categories!
+          </p>
+        </div>
+        <Typography
+          variant='h1'
+          className='bg-clip-text font-extrabold [text-wrap:balance] sm:text-5xl'
+        >
+          Streamline Your{" "}
+          <span className='bg-gradient-to-br from-[#4cc9f0] to-[#4361ee] bg-clip-text text-transparent'>
+            Rust Base Automation
+          </span>{" "}
+          with Ease
         </Typography>
-        <p className='mt-6 text-lg text-muted-foreground'>
+        <p className='text-muted-foreground mt-6 text-lg'>
           Tired of spending hours setting up conveyor filters every wipe? Our
           application lets you create, edit, and share custom conveyor setups
           effortlessly. Browse a public directory, save your favorite filters,
@@ -37,9 +56,11 @@ export function HeroContent() {
               </SignUpButton>
             </Button>
           </SignedOut>
-          <Button type='button' variant='link'>
-            <Link href='/filters'>Browse Filters</Link>
-            <ArrowRight />
+          <Button type='button' variant='link' className='group' asChild>
+            <Link href='/filters'>
+              Browse Filters
+              <ArrowRight className='transition-transform group-hover:translate-x-0.5' />
+            </Link>
           </Button>
         </div>
         <div className='mt-8'>

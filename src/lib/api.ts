50b431475dbce8ab1af612e97130fdcdf6ece6df
Daniel Tostes <tostes.dev@gmail.com>
2025-02-12 16:34:58 -0300

    refactor(api): update Vercel environment variable references
    - Remove `NEXT_PUBLIC_` prefix from Vercel environment variables

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,18 +6,15 @@
  */
 export function getApiUrl(endpoint: string): string {
   const environment: string | undefined =
-    process.env.NEXT_PUBLIC_VERCEL_ENV ?? process.env.NODE_ENV;
-
+    process.env.VERCEL_ENV ?? process.env.NODE_ENV;
   if (environment === "production") {
-    const productionUrl = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
+    const productionUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL;
     return `https://${productionUrl}/api/${endpoint}`;
   }
   if (environment === "preview") {
-    const vercelUrl: string | undefined = process.env.NEXT_PUBLIC_VERCEL_URL;
+    const vercelUrl: string | undefined = process.env.VERCEL_URL;
     if (!vercelUrl) {
-      throw new Error(
-        "NEXT_PUBLIC_VERCEL_URL is not defined in preview environment",
-      );
+      throw new Error("VERCEL_URL is not defined in preview environment");
     }
     return `https://${vercelUrl}/api/${endpoint}`;
   }

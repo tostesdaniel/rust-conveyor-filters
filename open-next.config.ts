101567873eaf85aa409b1cc4200b5c1917f12842
Daniel Tostes <tostes.dev@gmail.com>
2025-05-17 15:50:50 -0300

    refactor: revert migration changes
    - Revert most changes from cloudflare migration

--- open-next.config.ts
+++
@@ -1,9 +0,0 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
-import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
-
-export default defineCloudflareConfig({
-  incrementalCache: kvIncrementalCache,
-  queue: doQueue,
-});

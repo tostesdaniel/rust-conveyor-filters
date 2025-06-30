2406b21f90ee41381124a9127d328c2de55c227b
Daniel Tostes <tostes.dev@gmail.com>
2025-06-30 15:24:43 -0300

    feat(filters-sidebar-content): add FiltersSidebarContent component to include category selection

---
+++ src/components/filters/sidebar/filters-sidebar-content.tsx
@@ -0,0 +1,9 @@
+import { CategorySelection } from "@/components/filters/sidebar/category-selection";
+
+export function FiltersSidebarContent() {
+  return (
+    <>
+      <CategorySelection />
+    </>
+  );
+}

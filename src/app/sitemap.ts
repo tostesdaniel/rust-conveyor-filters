7d5cf14536f58a7bafe54baf368b583d2f8d46f9
Daniel Tostes <tostes.dev@gmail.com>
2025-07-05 20:28:02 -0300

    fix(sitemap): add routes for steam guide, privacy policy, and terms of service
    - Included '/steam-guide' in the sitemap for better navigation.
    - Added '/privacy-policy' and '/terms-of-service' with specific last modified dates to enhance compliance and user information.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,11 +5,21 @@ import { siteConfig } from "@/config/site";
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = `${siteConfig.url}`;
   const lastModified = new Date().toISOString();
+
   return [
     { url: baseUrl, lastModified },
     { url: `${baseUrl}/filters`, lastModified },
     { url: `${baseUrl}/donate`, lastModified },
     { url: `${baseUrl}/feedback`, lastModified },
     { url: `${baseUrl}/about`, lastModified },
+    { url: `${baseUrl}/steam-guide`, lastModified },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date("2024-08-24").toISOString(),
+    },
+    {
+      url: `${baseUrl}/terms-of-service`,
+      lastModified: new Date("2024-08-24").toISOString(),
+    },
   ];
 }

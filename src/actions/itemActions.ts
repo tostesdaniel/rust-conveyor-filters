5abfc6217b950df1b9729007b942f07b18df0f13
Daniel Tostes <tostes.dev@gmail.com>
2025-07-09 17:58:35 -0300

    feat(itemActions): implement unstable_cache for fetching items with revalidation

--- src/actions/itemActions.ts
+++ src/actions/itemActions.ts
@@ -1,11 +1,23 @@
 "use server";
 
+import { unstable_cache } from "next/cache";
 import { db } from "@/db";
 import { asc } from "drizzle-orm";
 
 import { items } from "@/db/schema";
 
-export async function getItems() {
+export const getItems = unstable_cache(
+  async () => {
+    return await fetchItems();
+  },
+  ["items"],
+  {
+    revalidate: 24 * 60 * 60,
+    tags: ["items"],
+  },
+);
+
+async function fetchItems() {
   try {
     const result = await db
       .select()

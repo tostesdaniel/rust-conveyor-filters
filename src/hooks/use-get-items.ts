14cca280d744517082f7dad6e205bd79406e2301
Daniel Tostes <tostes.dev@gmail.com>
2025-07-09 18:05:46 -0300

    feat(useGetItems): enhance query configuration revalidation

--- src/hooks/use-get-items.ts
+++ src/hooks/use-get-items.ts
@@ -6,5 +6,9 @@ export function useGetItems() {
   return useQuery({
     queryKey: ["items"],
     queryFn: getItems,
+    staleTime: 4 * 60 * 60 * 1000,
+    gcTime: 24 * 60 * 60 * 1000,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
   });
 }

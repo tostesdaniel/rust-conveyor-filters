54387a57da1a34c67014c25b9b40a0a23285a767
Daniel Tostes <tostes.dev@gmail.com>
2025-07-23 05:52:27 -0300

    feat: add testimonial data

---
+++ src/config/testimonials.ts
@@ -0,0 +1,18 @@
+export type Testimonial = {
+  name: string;
+  memberId: string;
+  designation: string;
+  title: string;
+  quote: string;
+};
+
+export const testimonials: Testimonial[] = [
+  {
+    name: "Jumble Jam Jam",
+    memberId: "1357420469107687434",
+    designation: "Solo Player",
+    title: "Cool & Useful",
+    quote:
+      "Before using this tool I had to stress about adding filters effectively, but now with this tool I am able to make effective auto sorters and even auto smelters, plus more using the filters. This tool helped me so much.",
+  },
+];

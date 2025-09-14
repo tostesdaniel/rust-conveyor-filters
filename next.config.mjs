34ec939d971f551055e0e4db1a764be4e15fa794
Daniel Tostes <tostes.dev@gmail.com>
2025-09-14 00:36:13 -0300

    refactor(next.config): remove standalone output configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
